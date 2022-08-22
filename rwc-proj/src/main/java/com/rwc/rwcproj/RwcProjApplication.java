2e1f9dff7705cbe83c1649f3059f1f8b87c74040
Shawn <bluesfansince2013@gmail.com>
2022-08-21 19:59:24 -0500

    initial backend commit

---
+++ rwc-proj/src/main/java/com/rwc/rwcproj/RwcProjApplication.java
@@ -0,0 +1,13 @@
+package com.rwc.rwcproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwcProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RwcProjApplication.class, args);
+    }
+
+}
---
+++ rwc-proj/src/test/java/com/rwc/rwcproj/RwcProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwc.rwcproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwcProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
