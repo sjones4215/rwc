30510f30c2f3ba78da5a06f4517eaecc19a48d99
Shawn <bluesfansince2013@gmail.com>
2022-08-23 23:33:21 -0500

    Modularizing NavBar.tsx

---
+++ rwc-ui/src/view/HomePage_utillity_components/NavBar.tsx
@@ -0,0 +1,33 @@
+import {AppBar, Box, Button, IconButton, Toolbar, Typography} from "@mui/material";
+
+const navItems = ['Home', 'About', 'Contact', 'Events'];
+
+export const NavBar = () => {
+    return (
+        <AppBar component="nav" className="app-bar">
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="start"
+                    sx={{ mr: 2, display: { sm: 'none' } }}
+                >
+                </IconButton>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                >
+                    MUI
+                </Typography>
+                <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                    {navItems.map((item) => (
+                        <Button key={item} sx={{ color: '#fff' }}>
+                            {item}
+                        </Button>
+                    ))}
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
+}
--- rwc-ui/src/view/HomeScreen.tsx
+++ rwc-ui/src/view/HomeScreen.tsx
@@ -1,37 +1,13 @@
 import '../styles/HomePage.scss'
-import {AppBar, Box, Button, IconButton, Toolbar, Typography} from "@mui/material";
+import {NavBar} from "./HomePage_utillity_components/NavBar";
+
 
 const HomePage = () => {
-    const navItems = ['Home', 'About', 'Contact', 'Events'];
-    return (
 
-        <span>
-            <AppBar component="nav" className="app-bar">
-        <Toolbar>
-          <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-          </IconButton>
-          <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MUI
-          </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map((item) => (
-                <Button key={item} sx={{ color: '#fff' }}>
-                    {item}
-                </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-        </span>
+    return (
+        <div>
+            <NavBar />
+        </div>
     );
 }
 
